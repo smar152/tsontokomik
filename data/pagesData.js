7e18b7fd960b3fd35d3744c86e9e6f6650794c78
smar152 <smar152@users.noreply.github.com>
2021-04-02 20:58:11 +0300

    Update pagesData.js

--- data/pagesData.js
+++ data/pagesData.js
@@ -1,60 +1,27 @@
-import Kickstarter from "../Components/Content/Kickstarter";
-
 export const pages = {
   home: {
     slug: "",
     title: "Smutcomic",
-    content: (
-      <div>
-        <p>hi</p>
-      </div>
-    ),
   },
   kickstarter: {
     slug: "kickstarter",
     title: "Kickstarter",
-    content: <Kickstarter />,
   },
   smutcomic1: {
     slug: "smutcomic1",
     title: "smutcomic1",
-    content: (
-      <div>
-        <p>hi</p>
-      </div>
-    ),
   },
   smutcomic2: {
     slug: "smutcomic2",
     title: "smutcomic2",
-    content: (
-      <div className="">
-        <p>hi</p>
-      </div>
-    ),
   },
   artists: {
     slug: "artists",
     title: "artists",
-    content: (
-      <div>
-        <p>hi</p>
-      </div>
-    ),
   },
   faq: {
     slug: "faq",
     title: "FAQ",
-    kati: {
-      en: "FAQ",
-      el: "ΦΑΚ",
-    },
-    content: (
-      <div>
-        <div className="faq">Πώς γυναίκες;!</div>
-        <div className="faa">Κι όμως!</div>
-      </div>
-    ),
   },
 };
 
